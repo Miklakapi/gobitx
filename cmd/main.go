7e0244b43da3c09213a29c6defe5a5e85e8be1c4
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-06-11 22:07:21 +0200

    first connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"os"
 	"time"
 )
@@ -12,16 +15,17 @@ type Config struct {
 	Mode        string
 	Destination string
 	Duration    time.Duration
-	Port        int
+	Port        string
 }
 
 func main() {
 	config := parseConfig()
 
-	fmt.Println("mode:", config.Mode)
-	fmt.Println("duration:", config.Duration)
-	fmt.Println("port:", config.Port)
-	fmt.Println("destination:", config.Destination)
+	if config.Mode == "server" {
+		runServer(config)
+		return
+	}
+	runClient(config)
 }
 
 func parseConfig() Config {
@@ -61,6 +65,48 @@ func parseConfig() Config {
 		Mode:        mode,
 		Destination: destination,
 		Duration:    duration,
-		Port:        port,
+		Port:        fmt.Sprint(":", port),
+	}
+}
+
+func runServer(cfg Config) {
+	l, err := net.Listen("tcp", cfg.Port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		go func(c net.Conn) {
+			defer c.Close()
+
+			reader := io.TeeReader(c, os.Stdout)
+
+			_, err := io.Copy(c, reader)
+			if err != nil {
+				log.Println("Connection error: ", err)
+			}
+		}(conn)
+	}
+}
+
+func runClient(cfg Config) {
+	var d net.Dialer
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	conn, err := d.DialContext(ctx, "tcp", fmt.Sprint("localhost", cfg.Port))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+
+	if _, err := conn.Write([]byte("Test 123 312 Test")); err != nil {
+		log.Fatalln(err)
 	}
 }
