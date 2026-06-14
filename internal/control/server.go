4d0ebb7517ff2d8b9642c085c7b4caa33f7e8921
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-06-14 12:40:30 +0200

    base server

--- internal/control/server.go
+++ internal/control/server.go
@@ -1,1 +1,90 @@
 package control
+
+import (
+	"errors"
+	"log/slog"
+	"net"
+	"sync"
+
+	"github.com/Miklakapi/gobitx/internal/config"
+	"github.com/Miklakapi/gobitx/internal/protocol"
+)
+
+type Server struct {
+	cfg             config.Config
+	listener        net.Listener
+	clientConnected bool
+	mu              sync.Mutex
+}
+
+func NewServer(cfg config.Config) (*Server, error) {
+	listener, err := net.Listen("tcp", cfg.Port)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Server{
+		cfg:             cfg,
+		listener:        listener,
+		clientConnected: false,
+	}, nil
+}
+
+func (s *Server) Run() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if errors.Is(err, net.ErrClosed) {
+				return
+			}
+			slog.Error("accept error: ", "err", err)
+			continue
+		}
+
+		go s.handleConnection(conn)
+	}
+}
+
+func (s *Server) Close() {
+	s.listener.Close()
+}
+
+func (s *Server) handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	codec := protocol.NewCodec(conn)
+
+	s.mu.Lock()
+	if s.clientConnected {
+		s.mu.Unlock()
+
+		slog.Debug("client rejected", "reason", "another client is already connected")
+
+		errorFrame, err := protocol.NewFrame(protocol.CommandError, protocol.ErrorPayload{
+			Code:    "server_busy",
+			Message: "server is busy",
+		})
+		if err != nil {
+			slog.Error("failed to build error frame", "err", err)
+			return
+		}
+
+		err = codec.WriteFrame(errorFrame)
+		if err != nil {
+			slog.Error("failed to write error frame", "err", err)
+			return
+		}
+
+		return
+	}
+	s.clientConnected = true
+	s.mu.Unlock()
+
+	defer func() {
+		s.mu.Lock()
+		s.clientConnected = false
+		s.mu.Unlock()
+	}()
+
+	slog.Debug("client connected")
+}
--- internal/protocol/frame.go
+++ internal/protocol/frame.go
@@ -1,5 +1,7 @@
 package protocol
 
+import "encoding/json"
+
 const (
 	ProtocolVersion uint8 = 1
 	HeaderSize            = 8
@@ -11,4 +13,22 @@ var ProtocolMagic = [2]byte{'G', 'B'}
 type Frame struct {
 	Command Command
 	Payload []byte
+}
+
+func NewFrame(command Command, payload any) (Frame, error) {
+	var jsonPayload []byte = nil
+
+	if payload != nil {
+		data, err := json.Marshal(payload)
+		if err != nil {
+			return Frame{}, err
+		}
+
+		jsonPayload = data
+	}
+
+	return Frame{
+		Command: command,
+		Payload: jsonPayload,
+	}, nil
 }
